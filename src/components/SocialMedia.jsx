2acd119b56378583b73f346a720295a823c75cc6
Josen Romero <romerojosemanuel@hotmail.com>
2022-11-02 20:24:56 -0600

    update SocialMedia component

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,7 +2,7 @@
 import { Button } from 'react-bootstrap';
 
 import Icon from '@mdi/react';
-import { mdiGithub, mdiInstagram, mdiEmail  } from '@mdi/js';
+import { mdiGithub, mdiLinkedin, mdiEmail  } from '@mdi/js';
 
 const SocialMedia = () => {
 
@@ -14,8 +14,8 @@ const SocialMedia = () => {
             <Button href="mailto:romerojosemanuel@hotmail.com" className="m-1" variant="outline-secondary">
                 <Icon path={mdiEmail} size={1}/>
             </Button>
-            <Button href="https://www.instagram.com/josen._.romero/" className="m-1" variant="outline-secondary" target="_blank">
-                <Icon path={mdiInstagram} size={1}/>
+            <Button href="https://www.linkedin.com/in/romerojosemanuel/" className="m-1" variant="outline-secondary" target="_blank">
+                <Icon path={mdiLinkedin} size={1}/>
             </Button>
         </div>
     );
