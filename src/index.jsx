5a9335dff2a9359d646d4b4f1f088fe4d1bf5199
Jose <romerojosemanuel@hotmail.com>
2023-11-19 18:26:20 -0600

    Migrate from create-react-app to Vite

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+import "./i18n/i18nConfig"
+
+import { ThemeProvider } from "./context/theme-context"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+)
