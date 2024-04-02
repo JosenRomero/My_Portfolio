c4300dc899d79e3b1731db0a76dd1e3e119e1b57
Jose <romerojosemanuel@hotmail.com>
2024-04-01 18:28:01 -0600

    fix: Navbar collapse

--- src/components/ItemLink.jsx
+++ src/components/ItemLink.jsx
@@ -1,13 +1,15 @@
+import { Nav } from 'react-bootstrap';
 import { Link } from "react-scroll";
 
+// react-bootstrap needs href to use Navbar collapse
 const ItemLink = ({ to, children}) => {
 
   return (
-    <li>
-      <Link activeClass="active" smooth spy offset={-25} to={to}>
-        {children}
-      </Link>
-    </li>
+    <Nav.Item as={"li"}>
+      <Nav.Link as={Link} href={to} activeClass="active" smooth spy offset={-25} to={to}>
+          {children}
+      </Nav.Link>
+    </Nav.Item>
   )
 
 }
