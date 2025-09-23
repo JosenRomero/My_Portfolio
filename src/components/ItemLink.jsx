0104f89510d8fdda0b9b51d1c98cc2740fd727fa
Jose <romerojosemanuel@hotmail.com>
2025-09-22 18:32:14 -0600

    feat(smooth-scroll): remove blueActive class on window resize

--- src/components/ItemLink.jsx
+++ src/components/ItemLink.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-scroll"
 const ItemLink = ({ to, spy, children }) => {
 	return (
 		<Nav.Item as={"li"}>
-			<Nav.Link as={Link} active={false} href={to} activeClass="blueActive" smooth={true} spy={spy} offset={-25} to={to} delay={0}>
+			<Nav.Link as={Link} active={false} href={to} activeClass={ spy ? "blueActive" : "" } smooth={true} spy={spy} offset={-25} to={to} delay={0}>
 				{children}
 			</Nav.Link>
 		</Nav.Item>
